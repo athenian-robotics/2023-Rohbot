e48d394a2bc5b528deab03fe067889ed5a7ad815
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2023-01-21 13:39:24 -0800

    Constants Updated

--- src/main/java/com/arc852/Constants.java
+++ src/main/java/com/arc852/Constants.java
@@ -19,15 +19,15 @@ public final class Constants {
     public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(21.73);
-    public static final double wheelBase = Units.inchesToMeters(21.73);
+    public static final double trackWidth = Units.inchesToMeters(17.75);
+    public static final double wheelBase = Units.inchesToMeters(17.75);
     public static final double wheelDiameter = Units.inchesToMeters(3.94);
     public static final double wheelCircumference = wheelDiameter * Math.PI;
 
     public static final double openLoopRamp = 0.25;
     public static final double closedLoopRamp = 0.0;
 
-    public static final double driveGearRatio = (6.86 / 1.0); // 6.86:1
+    public static final double driveGearRatio = (6.75 / 1.0); // 6.75:1
     public static final double angleGearRatio = (12.8 / 1.0); // 12.8:1
 
     public static final SwerveDriveKinematics swerveKinematics =
@@ -84,32 +84,32 @@ public final class Constants {
     /* Module Specific Constants */
     /* Front Left Module - Module 0 */
     public static final class Mod0 {
-      public static final int driveMotorID = 1;
-      public static final int angleMotorID = 2;
+      public static final int driveMotorID = 5;
+      public static final int angleMotorID = 6;
       public static final int canCoderID = 1;
       public static final double angleOffset = 37.35;
     }
 
     /* Front Right Module - Module 1 */
     public static final class Mod1 {
-      public static final int driveMotorID = 3;
-      public static final int angleMotorID = 4;
+      public static final int driveMotorID = 7;
+      public static final int angleMotorID = 8;
       public static final int canCoderID = 2;
       public static final double angleOffset = 10.45;
     }
 
     /* Back Left Module - Module 2 */
     public static final class Mod2 {
-      public static final int driveMotorID = 5;
-      public static final int angleMotorID = 6;
+      public static final int driveMotorID = 9;
+      public static final int angleMotorID = 10;
       public static final int canCoderID = 3;
       public static final double angleOffset = 38.75;
     }
 
     /* Back Right Module - Module 3 */
     public static final class Mod3 {
-      public static final int driveMotorID = 7;
-      public static final int angleMotorID = 8;
+      public static final int driveMotorID = 11;
+      public static final int angleMotorID = 12;
       public static final int canCoderID = 4;
       public static final double angleOffset = 58.88;
     }
